f7a4ff0d067faf3330db7f4e49d4551e82ce32f6
Kinoko <huiyuanai129@outlook.com>
2024-08-12 10:21:30 +0800

    vector container: assign

---
+++ 3.2.3.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printVector(vector<int> &v){
+    for(vector<int>::iterator it = v.begin(); it < v.end(); it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+}
+
+void test01(){
+    vector<int> v1;
+    for(int i = 0; i < 10; i++){
+        v1.push_back(i);
+    }
+
+    //operator=
+    vector<int> v2 = v1;
+    printVector(v2);
+
+    //assign
+    vector<int> v3;
+    v3.assign(v1.begin(),v1.end());     //左括号是闭区间 右括号是开区间
+    printVector(v3);
+
+    //assign
+    vector<int> v4;
+    v4.assign(10,100);
+    printVector(v4);
+}
+
+int main(){
+    
+    test01();
+
+    return 0;
+}
