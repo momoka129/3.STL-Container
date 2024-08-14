a3ed2a1d9b03fb2c88058da0ba65b69866d90c95
Kinoko <huiyuanai129@outlook.com>
2024-08-14 21:21:29 +0800

    map: basic concepts(pair:key, value), constructor

---
+++ 3.9.2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+void printMap(map<int, int> &m){
+    for(map<int, int>::iterator it = m.begin(); it != m.end(); it++){
+        cout<<it->first<<"\t"<<it->second<<endl;
+    }
+    cout<<endl;
+}
+
+void test01(){
+
+    map<int, int> m;
+
+    m.insert(pair<int, int>(1,10));
+    m.insert(pair<int, int>(11,6));
+    m.insert(pair<int, int>(1,30));
+
+    printMap(m);
+    //1     10
+    //11    6
+
+    map<int, int> m2(m);
+    printMap(m2);
+
+    map<int, int> m3;
+    m3 = m2;
+    printMap(m3);
+
+}
+
+int main(){
+
+    test01();
+
+    return 0;
+}
