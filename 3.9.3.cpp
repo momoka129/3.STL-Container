9f7cd16b8f19e8cac3fdb039009a5631b6fd7461
Kinoko <huiyuanai129@outlook.com>
2024-08-16 16:56:16 +0800

    map: empty, size, swap

--- 3.9.3.cpp
+++ 3.9.3.cpp
@@ -1,10 +1,49 @@
 #include<iostream>
-
 using namespace std;
+#include<map>
+
+void printMap(map<int,int> &m){
+    for(map<int,int>::iterator it = m.begin(); it != m.end(); it++){
+        cout<<"key = "<<it->first<<"\tvalue: "<<it->second<<endl;
+    }
+    cout<<endl;
+}
+
+void test01(){
+    map<int, int> m;
+
+    m.insert(pair<int,int>(1,10));
+    m.insert(pair<int,int>(2,10));
+    m.insert(pair<int,int>(3,10));
+
+    if(m.empty()){
+        cout<<"empty."<<endl;
+    }
+    else{
+        cout<<"not empty."<<endl;
+        cout<<"size: "<<m.size()<<endl;
+    }
+
+    map<int, int> m2;
+
+    m2.insert(pair<int,int>(6,190));
+    m2.insert(pair<int,int>(5,106));
+    m2.insert(pair<int,int>(4,101));
+
+    cout<<"before: "<<endl;
+    printMap(m);
+    printMap(m2);
+
+    m.swap(m2);
+
+    cout<<"after: "<<endl;
+    printMap(m);
+    printMap(m2);
+}
 
 int main(){
 
-    cout<<"服了。。。"<<endl;
+    test01();
 
     return 0;
 }
